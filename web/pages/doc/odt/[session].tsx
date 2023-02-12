5c560ed9a15bf3da009c32a4980fb84c3d6349e5
Nikola Jovanovic <nikolajovanovic5556@gmail.com>
2023-02-12 22:32:15 +0100

    init

--- web/pages/doc/odt/[session].tsx
+++ web/pages/doc/odt/[session].tsx
@@ -3,13 +3,13 @@ import dynamic from 'next/dynamic';
 import AppLayout from '@layout/AppLayout';
 import { NextPageWithLayout } from '@ts/global.types';
 
-import Odt from '@module/Odt/OdtPage';
-// const OdtWithNoSSR = dynamic(() => import('@module/Odt/Odt'), {
-//   ssr: false,
-// });
+// import Odt from '@module/Odt/OdtPage';
+const OdtWithNoSSR = dynamic(() => import('@module/Odt/OdtPage'), {
+  ssr: false,
+});
 
 const Page: NextPageWithLayout = () => {
-  return <Odt />;
+  return <OdtWithNoSSR />;
 };
 
 Page.getLayout = (page: React.ReactElement) => {
